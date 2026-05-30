fa442b08e40e0a995a704fd5bbcfba410ec5aca1
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-05-30 20:42:21 +0600

    feat: restructure app layout and add AppSidebar component

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,30 +1,18 @@
+import { AppSidebar } from './components/app-sidebar'
+import { Button } from './components/ui/button'
+
 export function App() {
   function ipcHandle(): void {
     return window.electron.ipcRenderer.send('ping')
   }
 
   return (
-    <>
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+    <div className="grid size-full grid-cols-[200px_1fr]">
+      <AppSidebar />
+
+      <div>
+        <Button onClick={ipcHandle}>Send IPC</Button>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
